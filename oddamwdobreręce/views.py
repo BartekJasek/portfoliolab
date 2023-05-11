584df86d7f473870b18e7bf09df4f20641d24b9c
BartekJasek <bartek.jasek5@gmail.com>
2023-05-11 20:49:08 +0200

    static files

--- oddamwdobreręce/views.py
+++ oddamwdobreręce/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def landingpage(request):
+    return render(request, 'index.html')
+
+
+def addddonation(request):
+    return render(request, 'form.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
--- portfoliolab/urls.py
+++ portfoliolab/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from oddamwdobreręce import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.landingpage),
+    path('login/', views.login),
+    path('register/', views.register),
+    path('adddonation/', views.addddonation),
 ]
