1abc2c712aa602d614941ea691c60bb3de92ad17
BartekJasek <bartek.jasek5@gmail.com>
2023-05-18 21:19:22 +0200

    fundations to template

--- oddamwdobreręce/views.py
+++ oddamwdobreręce/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
 from .models import Donation, Institution, Category, User
-from .forms import UserCreationForm
 
 # Create your views here.
 
